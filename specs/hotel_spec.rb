3be96a451c883309f457409139e04e138425b549
Salome Wubeshet <swubeshet@gmail.com>
2017-09-06 12:27:41 -0700

    new coverage Simplecov file, fixed error in spec files and passed an instance of Hotel class test

--- specs/hotel_spec.rb
+++ specs/hotel_spec.rb
@@ -1,2 +1,13 @@
-require_relative '../lib/reservation'
+require_relative '../lib/hotel.rb'
 require_relative 'spec_helper'
+
+describe "class Hotel"  do
+  before do
+    @hotel = Hotel::Hotel.new
+  end
+  describe "instance of Hotel class" do
+    it "creates an instance of hotel" do
+      @hotel.must_be_instance_of Hotel::Hotel
+    end
+  end
+end
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -1,2 +1,2 @@
-require_relative '../lib/hotel'
+require_relative '../lib/reservation'
 require_relative 'spec_helper'
