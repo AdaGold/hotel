6077cefea58563be5fe135400b19e5ab43752261
Salome Wubeshet <swubeshet@gmail.com>
2017-09-06 15:28:57 -0700

    update #rooms to return @rooms

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -1,7 +1,15 @@
-require_relative 'reservation.rb'
-require_relative 'date_range.rb'
+# require_relative 'reservation.rb'
+# require_relative 'date_range.rb'
+require 'date'
 module Hotel
   class Hotel
+    def initialize
+      @rooms = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    end
+
+    def rooms
+      return @rooms
+    end
 
   end #end of class
 end #end of module
