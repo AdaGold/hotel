b3805db734e979918b77f9b9e806333033009785
Salome Wubeshet <swubeshet@gmail.com>
2017-09-09 13:04:37 -0700

    spec test instance of DateRange class

--- specs/date_range_spec.rb
+++ specs/date_range_spec.rb
@@ -1,2 +1,13 @@
 require_relative '../lib/date_range'
 require_relative 'spec_helper'
+
+describe "class DateRange"  do
+  before do
+    @date_range = Hotel::DateRange.new
+  end
+  describe "instance of DateRange class" do
+    it "creates an instance of DateRange" do
+      @date_range.must_be_instance_of Hotel::DateRange
+    end
+  end
+end
