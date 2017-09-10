6454e8e60736df9fab54e88b119df2d0c0a682c2
Salome Wubeshet <swubeshet@gmail.com>
2017-09-09 18:50:11 -0700

    updated initialize test for DateRange in spec and initialize in DateRage file

--- specs/date_range_spec.rb
+++ specs/date_range_spec.rb
@@ -3,9 +3,9 @@ require_relative 'spec_helper'
 
 describe "class DateRange"  do
   before do
-    @date_range = Hotel::DateRange.new
+    @date_range = Hotel::DateRange.new(@room_number, @start_date, @end_date)
   end
-  describe "instance of DateRange class" do
+  describe "initialize" do
     it "creates an instance of DateRange" do
       @date_range.must_be_instance_of Hotel::DateRange
     end
