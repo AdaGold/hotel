73e48703214effe9f2f56136fb2a9424222904b1
Salome Wubeshet <swubeshet@gmail.com>
2017-09-09 19:35:35 -0700

    instance of Reservation class

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,8 +1,14 @@
 require_relative 'hotel.rb'
 require_relative 'date_range.rb'
+require 'date'
 
 module Hotel
   class Reservation
+
+    def initialize(room_number, check_in, check_out)
+      @date = DateRange.new(room_number, check_in, check_out).date
+      @room_availability = {}
+    end
 
   end #end of class
 end #end of module
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -1,2 +1,15 @@
 require_relative '../lib/reservation'
 require_relative 'spec_helper'
+
+describe "class DateRange"  do
+  before (:all) do
+    @check_in = Date.new(2017,10,01)
+    @check_out = Date.new(2017,10,10)
+    @reservation = Hotel::Reservation.new(3, @check_in, @check_out)
+  end
+ describe "initialize" do
+    it "creates an instance of Reservation" do
+      @reservation.must_be_instance_of Hotel::Reservation
+    end
+  end
+end
