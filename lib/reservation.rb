310fb8a524c55655226a8e8dfacea72b9b746be6
Salome Wubeshet <swubeshet@gmail.com>
2017-09-09 19:53:28 -0700

    added #total_cost and #price_night tests and methods to lib and spec files

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -4,10 +4,19 @@ require 'date'
 
 module Hotel
   class Reservation
+    attr_reader :date, :room_availability
 
     def initialize(room_number, check_in, check_out)
       @date = DateRange.new(room_number, check_in, check_out).date
       @room_availability = {}
+    end
+
+    def price_night
+      return 200
+    end
+
+    def total_cost
+      return price_night * date
     end
 
   end #end of class
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -1,15 +1,25 @@
 require_relative '../lib/reservation'
 require_relative 'spec_helper'
 
-describe "class DateRange"  do
+describe "class Reservation"  do
   before (:all) do
     @check_in = Date.new(2017,10,01)
-    @check_out = Date.new(2017,10,10)
+    @check_out = Date.new(2017,10,03)
     @reservation = Hotel::Reservation.new(3, @check_in, @check_out)
   end
- describe "initialize" do
+  describe "initialize" do
     it "creates an instance of Reservation" do
       @reservation.must_be_instance_of Hotel::Reservation
+    end
+  end
+  describe "#price_night" do
+    it "returns 200 (for each room)" do
+      @reservation.price_night.must_equal 200
+    end
+  end
+  describe "#total_cost" do
+    it "must return the total cost paid for length of stay" do
+      @reservation.total_cost.must_equal 400
     end
   end
 end
