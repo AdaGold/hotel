9b9bca553630d6c0f6a5d5d0239f7de1c837a414
Salome Wubeshet <swubeshet@gmail.com>
2017-09-09 19:23:43 -0700

    added require 'date', :date and @date, date method and wrote first date method test

--- specs/date_range_spec.rb
+++ specs/date_range_spec.rb
@@ -2,12 +2,24 @@ require_relative '../lib/date_range'
 require_relative 'spec_helper'
 
 describe "class DateRange"  do
-  before do
-    @date_range = Hotel::DateRange.new(@room_number, @start_date, @end_date)
+  before (:all) do
+    # @date_range = Hotel::DateRange.new(3, '2017,10,10', '2017,10,17')
+    @start_date = Date.new(2017,10,01)
+    @end_date = Date.new(2017,10,10)
+    @date_range = Hotel::DateRange.new(3, @start_date, @end_date)
   end
   describe "initialize" do
     it "creates an instance of DateRange" do
       @date_range.must_be_instance_of Hotel::DateRange
+    end
+  end
+  describe "#date" do
+    it "returns @date as an integer" do
+      # @date_range = Hotel::DateRange.new(3, @start_date, @end_date)
+      @start_date = Date.new(2017,10,01)
+      @end_date = Date.new(2017,10,10)
+      # @date_range = Hotel::DateRange.new(3,'2017,10,01', '2017,10,10')
+      @date_range.date.must_be_kind_of Integer
     end
   end
 end
