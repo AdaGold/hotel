c07ef2f63382d2c58636b8da5d125d892ad3946f
Hana <hanalways@gmail.com>
2019-03-12 19:25:23 -0700

    refactor tests with let

--- spec/hotelsystem_spec.rb
+++ spec/hotelsystem_spec.rb
@@ -1,18 +1,32 @@
 require_relative "spec_helper.rb"
 
 describe "HotelSystem class" do
-  it "creates an instance of HotelSystem" do
-    hotel = Hotel::HotelSystem.new
-    expect(hotel).must_be_kind_of Hotel::HotelSystem
+  let (:reservation) {
+    start_date = Date.new(2019, 3, 23)
+    end_date = start_date + 2
+    Hotel::Reservation.new(start_date, end_date)
+  }
+
+  let (:hotel_system) {
+    Hotel::HotelSystem.new
+  }
+
+  describe "HotelSystem instantiation" do
+    it "creates an instance of HotelSystem" do
+      hotel = Hotel::HotelSystem.new
+      expect(hotel).must_be_kind_of Hotel::HotelSystem
+    end
+
+    it "creates an array of rooms" do
+      hotel = Hotel::HotelSystem.new
+      expect(hotel.list_rooms).must_be_kind_of Array
+    end
   end
 
-  it "creates an array of rooms" do
-    hotel = Hotel::HotelSystem.new
-    expect(hotel.list_rooms).must_be_kind_of Array
-  end
-
-  it "lists 20 rooms" do
-    hotel = Hotel::HotelSystem.new
-    expect(hotel.list_rooms.length).must_equal 20
+  describe "HotelSystem lists all rooms" do
+    it "lists 20 rooms" do
+      hotel = Hotel::HotelSystem.new
+      expect(hotel.list_rooms.length).must_equal 20
+    end
   end
 end
