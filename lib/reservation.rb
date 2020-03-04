34aefad83c5179eacaf0a0bb2ac1cbd046a35d7a
Haben <haben7@gmail.com>
2020-03-03 18:56:53 -0800

    added methods

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,11 +1,15 @@
 module Hotel
   class Reservation
+    attr_reader :date_range
     # Feel free to change this method signature as needed. Make sure to update the tests!
     def initialize(start_date, end_date, room_number)
+      @date_range = DateRange.new(start_date, end_date)
+      @room_number = room_number
     end
-
+    
+  #get the total cost for a given reservation
     def cost
-      return 3
+      return @date_range.nights * 200
     end
   end
 end
