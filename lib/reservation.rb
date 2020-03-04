d1152156f97d98b449f0a9a3237e825f9ee0ffc8
Jocelyn Wang <wangjoc@gmail.com>
2020-03-04 09:23:08 -0800

    refactored reservation class

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -6,15 +6,14 @@ require_relative 'hotel'
 module HotelManager
 	class Reservation
 
-		attr_reader :id, :customer_id, :start_date, :end_date, :room_cost, :room, :room_id
-		# Initialize reservation with start/end date, cost (default $200), customer id
-		# Raise exception for invalid date range is provided 
+		attr_reader :id, :customer_id, :start_date, :end_date, :room_cost, 							:room, :room_id
+
 		def initialize (
 			id: , 
 			customer_id: , 
 			start_date: , 
 			end_date: , 
-			room_cost: 200, 
+			room_cost: 200, # Room default cost of $200
 			room: nil,
 			room_id: nil
 			)
@@ -25,62 +24,35 @@ module HotelManager
 			@end_date = end_date
 			@room_cost = room_cost
 			@room = room
+			@room_id = room_id
 
-			if room
-				@room = room
-				@room_id = room_id
-			elsif
-				@room_id = room_id
-			else
-				raise ArgumentError, "Room or room_id is required"
-			end
+			# Raise exception for missing or invalid inputs
 
-			if @start_date.class() != Date
-				raise ArgumentError, "Start date #{@start_date} not valid"
-			end  
+			raise ArgumentError, "Room or room_id is required" if room.nil? && room_id.nil?
 
-			if @end_date.class() != Date
-				raise ArgumentError, "End date #{@end_date} not valid"
-			end 
-			
-			if @start_date >= @end_date 
+			if @start_date.class() != Date || @end_date.class() != Date
+				raise ArgumentError, "Date #{@start_date} or #{@end_date} not valid"
+			elsif @start_date >= @end_date 
 				raise ArgumentError, "#{@start_date} is before #{@end_date} "
 			end
 		end
 
-		# Calculate cost of reservation (exclusive of last date)
+		# Calculate cost of reservation, exclusive of last date
 		def total_cost
 			@room_cost * (@end_date - @start_date - 1)
 		end
 		
-		# Check whether date is within range 
+		# Checks if reservation exists on a specific date 
 		def check_date(search_date)
 			(search_date >= @start_date) && (search_date <= @end_date)
 		end
 
-		# # Check whether date range overlaps with reservation
-		# def check_date_range(date_one, date_two)
-		# 	if date_one < date_two
-		# 		first_date, second_date = date_one, date_two
-		# 	else
-		# 		first_date, second_date = date_two, date_one
-		# 	end
-
-		# 	(first_date >= @start_date && first_date < @end_date) || (second_date < @end_date && second_date > @start_date) 
-		# end
-
-		# Check whether reservation is within range 
+		# Checks whether reservation is within a given date range 
 		def check_reservation_range(date_one, date_two)
-			if date_one < date_two
-				first_date, second_date = date_one, date_two
-			else
-				first_date, second_date = date_two, date_one
-			end
-
+			first_date = date_one < date_two ? date_one : date_two
+			second_date = date_one < date_two ? date_two : date_one
 			first_date < @end_date && second_date > @start_date
 		end
 
 	end
 end
-
-# Add in requirement that room must be populated
