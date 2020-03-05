e9ca15a98b7e4986542938d95b83b52a75a19982
Haben <haben7@gmail.com>
2020-03-04 17:46:18 -0800

    arranged simplcov in the right order

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,13 @@
 # Add simplecov
-require "minitest"
-require "minitest/autorun"
-require "minitest/reporters"
 require "simplecov"
 
 SimpleCov.start do
   add_filter 'test/'
 end
+require "minitest"
+require "minitest/autorun"
+require "minitest/reporters"
+
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
