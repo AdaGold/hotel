7d2e64960f99a0231e5c008ccb042fe5dc2fbcdd
Maryam Heshmati <mairheshmati@gmail.com>
2020-03-06 18:29:51 -0800

    finished wave 2- room has a method to return the dates it's reserved and a method to add room's reservation to it's own list

--- lib/room.rb
+++ lib/room.rb
@@ -8,18 +8,16 @@ module Hotel
     end
 
     def add_reservation_to_room(reservation)
+      raise(ArgumentError, "this reservation#{reservation} is not for #{@room_number}.") unless reservation.room == @room_number
       @room_reservations << reservation
     end
 
-    #return an array of all the dates room was booked/is unavailable 
+    #return an array of all the dates room was booked/is unavailable
     def dates_unavailable
-      dates_booked = @room_reservations.each do |reservation|
-        reservation.reservation_dates.all_dates.select do |day|
-          day
-        end
-        return dates_booked.flatten 
-
+      dates_booked = @room_reservations.map do |reservation|
+        reservation.reservation_dates.all_dates
       end
+      return dates_booked.flatten 
     end
   end
 end
