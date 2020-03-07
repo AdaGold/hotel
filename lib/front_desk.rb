ab3f7b182614fec221389f2494714aabf24b4028
Maryam Heshmati <mairheshmati@gmail.com>
2020-03-06 18:33:55 -0800

    finished wave 2- front desk methods include: making a reservation, seeing reservations for a specific date, and list of rooms available given a date range- tests are written but code base needs refactoring to dry up the code

--- lib/front_desk.rb
+++ lib/front_desk.rb
@@ -1,5 +1,7 @@
 module Hotel
   class FrontDesk
+    attr_reader :all_rooms, :all_reservations
+
     def initialize
       @all_rooms = []
       20.times do |i|
@@ -9,45 +11,44 @@ module Hotel
       @all_reservations = []
     end
 
-    #gives a copy of all rooms within the hotel
-    def list_of_all_rooms
-      return @all_rooms.clone
+    def list_of_available_rooms(start_date, end_date)
+      test_date_range = (start_date...end_date).to_a
+      return @all_rooms.reject do |room|
+               (room.dates_unavailable & test_date_range).length > 0
+             end
     end
 
-    #reserve a room by giving a date range, guest name, and which room to stay in
-    def reserve_room(start_date, end_date, guest_name, room)
-      raise(ArgumentError, "This room #{room} does not exist in our hotel:(") unless @all_rooms.include? room
-      raise(ArgumentError, "This room #{room} is unavailable for reservation for given dates #{start_date} - #{end_date}") unless list_of_availble_rooms(start_date, end_date).include? room 
+    def reserve_room(start_date, end_date, guest_name:, room:)
+        given_date_range = (start_date...end_date).to_a
+      raise(ArgumentError, "This room #{room} does not exist in our hotel:(") unless @all_rooms.any? do |individual_room|
+        individual_room.room_number == room
+      end
+      
 
-      new_res = Hotel::Reservation.new(start_date, end_date, guest_name: guest_name, room: room)
+      
+      reservation_room = @all_rooms.find do |room_object|
+        room_object.room_number == room
+      end
+      raise(ArgumentError, "This room #{room} is unavailable for reservation for given dates #{start_date} - #{end_date}") unless (reservation_room.dates_unavailable & given_date_range).length == 0
       #raise an argumentErro when the start_date and/or the  end_date of a reservation for a room already exits aka if the room is not available for those dates
 
-      reservation_room = @all_rooms.find do |room_object|
-        room_object == room
-      end
+
+      new_res = Hotel::Reservation.new(start_date, end_date, guest_name: guest_name, room: room)
       #add reservation to room's reservation_list
       reservation_room.add_reservation_to_room(new_res)
+
       #add this reservation to all_reservations in hotel
       @all_reservations << new_res
+      
+      return new_res
     end
 
-    #return an array of all Reservations with matching date requested
     def list_of_reservations(date)
       return @all_reservations.select do |reservation|
-               reservation.reservation_dates.all_dates.any? do |day| #this has high time complexity so might change it?
+               reservation.reservation_dates.all_dates.any? do |day|
                  date == day
                end
              end
-
-      return
-    end
-
-    #returns an array of all available rooms for the give date range 
-    def list_of_availble_rooms(start_date, end_date)
-      reserved_rooms = @all_rooms.select do |room|
-        room.dates_unavailable.any? (start_date...end_date)
-      end
-      return @all_rooms - reserved_rooms
     end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,7 @@ end
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
+require "minitest/skip_dsl" # skips the skips 
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
